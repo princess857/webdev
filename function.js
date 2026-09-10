8788ddb0c7bddd7a215070741b94aea20252a166
princess857 <chinweamadigwe12@gmail.com>
2026-09-10 12:25:48 +0100

    My Webpages

---
+++ function.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+// 
+//  const greet = ()=>console.log('Hello from arrow function!');
+ 
+//  greet();
+
+// 
+// const greet = (name)=>{
+//     console.log(`hello ${name} from arrow function!`);
+// }
+
+const greet = name => console.log(`Hello ${name} from arrow function!`);
+greet('John');
